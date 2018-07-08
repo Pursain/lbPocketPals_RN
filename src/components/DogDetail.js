84dc6f67dc203c6154c522e7d9637e4ab2c78453
Pursain <huangh101.0@gmail.com>
2018-07-07 17:15:14 -0700

    Refactored

--- src/components/DogDetail.js
+++ src/components/DogDetail.js
@@ -8,31 +8,43 @@ import Button from './Button';
 
 class DogDetail extends Component {
   state = {
-    breed: 'Click for more dogs!',
+    breed: 'Welcome, click below for more dogs!!!',
     image: './src/images/placeholder.jpg'
   };
 
   componentWillMount() {
+    //must be called componentWillMount to run on during mount time
+    //replaces placeholder image when app opens
     axios.get('https://dog.ceo/api/breeds/image/random').then(response => this.setState({ image: response.data.message }));
   }
 
-  fetchDog() {
-    //axios.get('https://dog.ceo/api/breeds/image/random').then(response => console.log(response.data.message));
-    //axios.get('https://dog.ceo/api/breeds/image/random').then(response => this.setState({ image: response.data.message }));
-    axios.get('https://dog.ceo/api/breeds/image/random').then(response => this.processDog(response.data.message));
+  getDogBreedAndImage(url) {
+    const breedRawArray = url.split('/')[4].split('-').reverse();
+    const breedFormated = this.formatDogBreed(breedRawArray);
 
-    //this.setState({ breed: 'dog' });
+    //Debug
+    //console.log(url);
+
+    this.setState({ breed: breedFormated, image: url });
   }
 
-  processDog(message) {
-    const breedRaw = message.split('/')[4].split('-').reverse();
+  formatDogBreed(breedRawArray) {
     const breedTemp = [];
-    for (let i = 0; i < breedRaw.length; i++) {
-      breedTemp[i] = breedRaw[i].charAt(0).toUpperCase() + breedRaw[i].substring(1);
+    for (let i = 0; i < breedRawArray.length; i++) {
+      breedTemp[i] = breedRawArray[i].charAt(0).toUpperCase() + breedRawArray[i].substring(1);
     }
-    const breedFormated = breedTemp.join(' ');
-    console.log(message);
-    this.setState({ breed: breedFormated, image: message });
+    return breedTemp.join(' ');
+  }
+
+  callDogApi() {
+    //seperate function bc .then() allows for async
+    //accounts for api call time
+
+    //Debug
+    //axios.get('https://dog.ceo/api/breeds/image/random').then(response => console.log(response.data.message));
+    //axios.get('https://dog.ceo/api/breeds/image/random').then(response => this.setState({ image: response.data.message }));
+
+    axios.get('https://dog.ceo/api/breeds/image/random').then(response => this.getDogBreedAndImage(response.data.message));
   }
 
   render() {
@@ -40,18 +52,18 @@ class DogDetail extends Component {
       <ScrollView>
         <Card>
           <CardSection>
-            <View style={{ flex: 1, alignItems: 'center' }}>
+            <View style={styles.breedViewStyle}>
               <Text style={styles.breedTextStyle}>{this.state.breed}</Text>
             </View>
           </CardSection>
           <CardSection>
             <Image
-              style={{ flex: 1, height: 300, width: null }}
+              style={styles.dogImageStyle}
               source={{ uri: this.state.image }}
             />
           </CardSection>
           <CardSection>
-            <Button onPress={() => this.fetchDog()}>Generate dog</Button>
+            <Button onPress={() => this.callDogApi()}>Click for more dogs</Button>
           </CardSection>
         </Card>
       </ScrollView>
@@ -60,8 +72,18 @@ class DogDetail extends Component {
 }
 
 const styles = {
+  breedViewStyle: {
+    flex: 1,
+    alignItems: 'center',
+  },
   breedTextStyle: {
     fontSize: 20,
+  },
+  dogImageStyle: {
+    flex: 1,
+    //TODO find appropriate height
+    height: 300,
+    width: null
   }
 };
 
